10f330b84318da6ddb6be247fd7825ea8315c28e
Jaurregos <161379015+Jaurregos@users.noreply.github.com>
2024-09-03 13:01:59 -0500

    ACTIVIDAD 5

---
+++ ACTIVIDAD5/Ejercicio8.2/src/Notas/Principal.java
@@ -0,0 +1,14 @@
+package Notas;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setResizable(false);
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setLocationRelativeTo(null);
+        
+    }
+    
+}
---
+++ ACTIVIDAD5/Ejercicio8.3/src/Figuras/Principal.java
@@ -0,0 +1,14 @@
+package Figuras;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+        
+    }
+    
+}

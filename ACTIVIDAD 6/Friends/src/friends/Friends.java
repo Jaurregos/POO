512af8af987bd3728996aa57f8cf02e053103c22
Jaurregos <161379015+Jaurregos@users.noreply.github.com>
2024-09-18 19:39:33 -0500

    ACTIVIDAD 6

---
+++ ACTIVIDAD 6/Friends/src/friends/Friends.java
@@ -0,0 +1,13 @@
+package friends;
+
+public class Friends {
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setResizable(false);
+        ventana.setVisible(true);
+        
+    }
+    
+}
